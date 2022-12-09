d7d6722f9ee34eb9bdd4c804fe766e5e973f7085
Vlad <Vlad@DESKTOP-MKLGP76>
2022-12-08 21:42:11 -0600

    Agregar archivos de proyecto.

---
+++ CRUDMVC/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDMVC.Models
+{
+    public partial class Empleado
+    {
+        public int Id { get; set; }
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Direccion { get; set; }
+        public string? NumeroTelefono { get; set; }
+    }
+}
